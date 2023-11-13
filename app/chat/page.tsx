f31702ffa9d315c306de3ec858f673ee26ef079d
Anas Alazhari <aalazhar@e1r6p11.1337.ma>
2023-11-13 20:07:47 +0100

    update

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -5,9 +5,14 @@ export default function chat() {
   return (
           <div className="flex flex-col text-slate-100 h-screen w-full">
             <div className=""><Navbar pageName="chat"/></div>
-            <div className="border h-[87%] m-2 ">
-              <div>
+            <div className=" w-[90%] h-[87%] m-auto">
+              <div className="w-full h-full flex justify-between items-center ">
+                <div className="w-[35%] h-full bg-[#323232] rounded-xl">
                 
+                </div>
+                <div className="w-[60%] h-full bg-[#323232] rounded-xl">
+  
+                </div>
               </div>
             </div>
           </div>
